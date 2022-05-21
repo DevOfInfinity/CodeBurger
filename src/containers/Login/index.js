f812f385c5e44e551f7e29826389ef246369b010
Joao Nascimento <devnascimento@hotmail.com>
2022-05-21 19:14:16 +0100

    Page Login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React from 'react'
+
+import LoginImg from '../../assets/Img-Login.png'
+import Logo from '../../assets/Code-Club-Burger.png'
+import {
+    Container,
+    LoginImage,
+    ContainerItens,
+    Label,
+    Input,
+    Button,
+    SignInLink
+} from './styles'
 
 function Login() {
     return (
-        <div>
-            <h1>Login Caralho 2</h1>
-        </div>
+        <Container>
+            <LoginImage src={LoginImg} alt='Login-Image' />
+            <ContainerItens>
+                <img src={Logo} alt='Logo-Image' />
+                <h1>Login</h1>
+
+                <Label>Email</Label>
+                <Input />
+
+                <Label>Senha</Label>
+                <Input />
+
+                <Button>Sign In</Button>
+                <SignInLink>Não possui conta ? <a>Sigin Up</a></SignInLink>
+            </ContainerItens>
+        </Container>
     )
 }
 
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -1,16 +1,95 @@
-/* import styled from 'styled-components'
+import styled from 'styled-components'
+import Background from '../../assets/Fundo.svg'
 
-export const LoginImage = styled.img`
-    height: 100%;
-    width: 60%;
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background: url('${Background}');
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
-export const ContainerItens = styled.div``
+export const LoginImage = styled.img`
+    height: 70%;
+`
 
-export const Label = styled.p``
+export const ContainerItens = styled.div`
+    background: #373737;
+    border-radius: 0 10px 10px 0;
+    height: 70%;
+    padding: 25px 75px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
 
-export const Input = styled.input``
+h1 {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+    color: #fff;
+    text-align: center;
+    margin-top: 100px;
+}
+`
 
-export const Button = styled.button``
+export const Label = styled.p`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    color: #FFFFFF;
+    margin: 28px 0 5px 0;
+`
 
-export const SignInLink = styled.p`` */
+export const Input = styled.input`
+    width: 391.42px;
+    height: 38.32px;
+    left: 944px;
+    top: 462.38px;
+    background: #FFFFFF;
+    box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
+    border-radius: 5px;
+    border: none;
+    padding-left: 10px;
+`
+
+export const Button = styled.button`
+    width: 182.81px;
+    height: 36.13px;
+    background: #9758A6;
+    border-radius: 20px;
+    border: none;
+
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    text-align: center;
+    color: #EEEEEE;
+
+    margin: 75px 0 20px 0;
+    cursor: pointer;
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.6;
+    }
+`
+
+export const SignInLink = styled.p`
+    font-style: normal;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 16px;
+    color: #FFFFFF;
+
+a {
+    cursor: pointer;
+    text-decoration: underline;
+}
+`
