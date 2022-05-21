b95cef643ba44a0763495df94dd331eabc14cd67
Joao Nascimento <devnascimento@hotmail.com>
2022-05-21 17:36:11 +0100

    CodeBurger

---
+++ src/containers/Login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+            <h1>Login Caralho 2</h1>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,16 @@
+/* import styled from 'styled-components'
+
+export const LoginImage = styled.img`
+    height: 100%;
+    width: 60%;
+`
+
+export const ContainerItens = styled.div``
+
+export const Label = styled.p``
+
+export const Input = styled.input``
+
+export const Button = styled.button``
+
+export const SignInLink = styled.p`` */
